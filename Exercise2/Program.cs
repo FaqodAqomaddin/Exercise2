94767c7c697a7892900dcc8c3aa135b4973a7511
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-11-04 16:57:30 +0700

    Add main function

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -94,3 +94,20 @@ namespace Exercise2
                 k++;
             }
         }
+        void sort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                
+                int m = l + (r - l) / 2;
+
+                
+                sort(arr, l, m);
+                sort(arr, m + 1, r);
+
+               
+                merge(arr, l, m, r);
+            }
+        }
+    }
+}
