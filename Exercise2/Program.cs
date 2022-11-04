96488ba05c8a288224792bf75224b3e16d0de0e4
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-11-04 16:54:57 +0700

    Add reaming elements mergesort

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -81,3 +81,16 @@ namespace Exercise2
                 }
                 k++;
             }
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
