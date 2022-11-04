a789d2f85dae796cf4d01ef87b42f5cc8f43d4ab
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-11-04 16:45:20 +0700

    Add Method 1 Sort

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -30,5 +30,24 @@ namespace Exercise2
                 arr[i] = Int32.Parse(s1);
             }
         }
+        static void sort(int[] arr)
+        {
+            int n = arr.Length;
+
+            
+            for (int i = 0; i < n - 1; i++)
+            {
+                
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+
+                
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+        }
     }
 }
